ff3b7cca2248160ac08708d83a885588d7ea02e3
chetnya69 <chetnyagupta77@gmail.com>
2020-03-10 11:27:28 +0530

    made new demo for adding and deleting a row using jquery

---
+++ js/add_delete_row_jquery.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $("#add").on("click", function () {
+        var row = $("#mytable").val();
+        $("#tablebody").append('<tr>'+
+            '<td>Chetnya</td>' +
+            '<td>Gupta</td>' +
+            '<td>21</td>' + 
+            '<td><button id="btnDelete" onclick="deleteRow(this);">Delete Row</button></td>' +
+        '</tr>');
+    });
+
+});
+
+function deleteRow(obj) {
+    $(obj).parent().parent().remove();
+}
